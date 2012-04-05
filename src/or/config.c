8333f29ab90b0383d915c9b965445b6195f575da
meejah <meejah@meejah.ca>
2012-04-05 15:17:30 -0600

    add new GETINFO config/defaults
    returns the default values for every configuration item, similar
    to GETINFO config/names; include a changes entry for it.
    
    Fix for bug 4971

--- src/or/config.c
+++ src/or/config.c
@@ -7037,6 +7037,20 @@ getinfo_helper_config(control_connection_t *conn,
     SMARTLIST_FOREACH(sl, char *, c, tor_free(c));
     smartlist_free(sl);
   }
+  else if (!strcmp(question, "config/defaults"))
+  {
+    smartlist_t *sl = smartlist_new();
+    int i;
+    for (i = 0; _option_vars[i].name; ++i) {
+      const config_var_t *var = &_option_vars[i];
+      char *val = esc_for_log(var->initvalue);
+      smartlist_add_asprintf(sl, "%s %s\n",var->name,val);
+      tor_free(val);
+    }
+    *answer = smartlist_join_strings(sl, "", 0, NULL);
+    SMARTLIST_FOREACH(sl, char *, c, tor_free(c));
+    smartlist_free(sl);
+  }
   return 0;
 }
 
